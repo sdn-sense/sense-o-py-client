c5cce2aa69e02bef30999cfa4d0e6f98f7851270
Justas Balcas <juztas@gmail.com>
2024-05-16 09:01:11 -0500

    Add Debug option for http requests. Add timeout for requests (def 60s)

--- sense/client/apiclient.py
+++ sense/client/apiclient.py
@@ -2,19 +2,19 @@ import os
 import json
 import requests
 from yaml import load as yload
-from yaml import FullLoader
 
 requests.packages.urllib3.disable_warnings()
 
-class ApiClient(object):
+class ApiClient():
+    """API Client for SENSE-0 get Token and Config"""
     def __init__(self):
         # For now only pass config file; Later all params
         self.token, self.config = None, None
         self.getConfig()
         self._getToken()
-        pass
 
     def _getToken(self):
+        """Get Token from SENSE-0 Auth API"""
         data = {'grant_type': 'password',
                 'username': self.config['USERNAME'],
                 'password': self.config['PASSWORD']}
@@ -22,21 +22,25 @@ class ApiClient(object):
                                       data=data,
                                       verify=self.config['verify'],
                                       allow_redirects=self.config['allow_redirects'],
-                                      auth=(self.config['CLIENT_ID'], self.config['SECRET']))
+                                      auth=(self.config['CLIENT_ID'], self.config['SECRET']),
+                                      timeout=os.environ.get('SENSE_TIMEOUT', 60))
         self.token = json.loads(tokenResponse.text)
         if 'error' in self.token.keys() and 'error_description' in self.token.keys():
-            raise Exception("Failed to get token. Bad credentials? Error: %s" % self.token['error_description'])
+            raise Exception(f"Failed to get token. Bad credentials? Error: {self.token['error_description']}")
         self._setHeaders()
 
     def _setHeaders(self):
+        """Set Headers for API calls"""
         self.config['headers'] = {'Content-type': 'application/json', 'Accept': 'application/json',
                                   'Authorization': 'Bearer ' + self.token['access_token']}
 
     def _refreshToken(self):
+        """Refresh Token from SENSE-0 Auth API"""
         self.getConfig()
         self._getToken()
 
     def _setDefaults(self):
+        """Set Defaults for Config"""
         for key, val in {'verify': False,
                          'allow_redirects': False,
                          'REST_API': self.config['API_ENDPOINT']}.items():
@@ -44,21 +48,22 @@ class ApiClient(object):
                 self.config[key] = val
 
     def _validateConfig(self):
+        """Validate Config"""
         for param in ['AUTH_ENDPOINT', 'API_ENDPOINT', 'USERNAME', 'PASSWORD', 'CLIENT_ID', 'SECRET']:
             if param not in self.config.keys():
-                raise Exception('Config parameter %s is not set' % param)
+                raise Exception(f"Config parameter {param} is not set")
 
     def getConfig(self, configFile='/etc/sense-o-auth.yaml'):
         """Get SENSE Auth config file"""
         if os.getenv('SENSE_AUTH_OVERRIDE'):
             configFile = os.getenv('SENSE_AUTH_OVERRIDE')
             if not os.path.isfile(configFile):
-                raise Exception('SENSE_AUTH_OVERRIDE env flag set, but file not found: %s' % configFile)
+                raise Exception(f"SENSE_AUTH_OVERRIDE env flag set, but file not found: {configFile}")
         elif not os.path.isfile(configFile):
             configFile = os.getenv('HOME') + '/.sense-o-auth.yaml'
             if not os.path.isfile(configFile):
-                raise Exception('Config file not found: %s' % configFile)
-        with open(configFile, 'r') as fd:
-            self.config = yload(fd.read(), Loader=FullLoader)
+                raise Exception(f"Config file not found: {configFile}")
+        with open(configFile, 'r', encoding="utf-8") as fd:
+            self.config = yload(fd.read())
         self._validateConfig()
         self._setDefaults()
--- sense/client/requestwrapper.py
+++ sense/client/requestwrapper.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
+"""Request Wrapper for SENSE-0 API"""
+import os
 import sys
 
+import logging
+from http.client import HTTPConnection
 import requests
-
 from sense.client.apiclient import ApiClient
 
 sys.path.insert(0, '..')
@@ -9,20 +13,43 @@ sys.path.insert(0, '..')
 
 class RequestWrapper(ApiClient):
     def __init__(self):
+        self.debug = False
+        self.__setdebug()
         super(RequestWrapper, self).__init__()
+
+    def __setdebug(self):
+        """Set Debug and log all http call details to console"""
+        if not os.environ.get('SENSE_FULL_DEBUG'):
+            log = logging.getLogger('RequestWrapperSense')
+            log.setLevel(logging.DEBUG)
+            ch = logging.StreamHandler()
+            ch.setLevel(logging.DEBUG)
+            if ch in log.handlers:
+                log.addHandler(ch)
+            self.debug = False
+            return
+        if not self.debug:
+            log = logging.getLogger('RequestWrapperSense')
+            log.setLevel(logging.DEBUG)
+            ch = logging.StreamHandler()
+            ch.setLevel(logging.DEBUG)
+            if ch not in log.handlers:
+                log.removeHandler(ch)
+            HTTPConnection.debuglevel = 0
+            self.debug = True
 
     def _get(self, api_path, params):
         url = self.config['REST_API'] + api_path
         out = requests.get(url,
                            headers=self.config['headers'],
                            verify=self.config['verify'],
-                           params=params)
+                           params=params, timeout=os.environ.get('SENSE_TIMEOUT', 60))
         if out.status_code == 401:
             self._refreshToken()
             out = requests.get(url,
                                headers=self.config['headers'],
                                verify=self.config['verify'],
-                               params=params)
+                               params=params, timeout=os.environ.get('SENSE_TIMEOUT', 60))
         return out
 
     def _put(self, api_path, data, params):
@@ -31,14 +58,14 @@ class RequestWrapper(ApiClient):
                            headers=self.config['headers'],
                            verify=self.config['verify'],
                            data=data,
-                           params=params)
+                           params=params, timeout=os.environ.get('SENSE_TIMEOUT', 60))
         if out.status_code == 401:
             self._refreshToken()
             out = requests.put(url,
                                headers=self.config['headers'],
                                verify=self.config['verify'],
                                data=data,
-                               params=params)
+                               params=params, timeout=os.environ.get('SENSE_TIMEOUT', 60))
         return out
 
     def _post(self, api_path, data, params):
@@ -47,14 +74,14 @@ class RequestWrapper(ApiClient):
                             headers=self.config['headers'],
                             verify=self.config['verify'],
                             data=data,
-                            params=params)
+                            params=params, timeout=os.environ.get('SENSE_TIMEOUT', 60))
         if out.status_code == 401:
             self._refreshToken()
             out = requests.post(url,
                                 headers=self.config['headers'],
                                 verify=self.config['verify'],
                                 data=data,
-                                params=params)
+                                params=params, timeout=os.environ.get('SENSE_TIMEOUT', 60))
         return out
 
     def _delete(self, api_path, params):
@@ -62,16 +89,18 @@ class RequestWrapper(ApiClient):
         out = requests.delete(url,
                               headers=self.config['headers'],
                               verify=self.config['verify'],
-                              params=params)
+                              params=params, timeout=os.environ.get('SENSE_TIMEOUT', 60))
         if out.status_code == 401:
             self._refreshToken()
             out = requests.delete(url,
                                   headers=self.config['headers'],
                                   verify=self.config['verify'],
-                                  params=params)
+                                  params=params, timeout=os.environ.get('SENSE_TIMEOUT', 60))
         return out
 
     def request(self, call_type, api_path, **kwargs):
+        """Request Wrapper for SENSE-0 API (GET, PUT, POST, DELETE)"""
+        self.__setdebug()
         ret = None
         params = None
         if kwargs.get('query_params'):
@@ -86,7 +115,7 @@ class RequestWrapper(ApiClient):
                 ret = self._post(api_path, kwargs.get('body_params'), params)
             else:
                 raise ValueError(
-                    "Missing the body parameter for POST to '%s'" % api_path)
+                    f"Missing the body parameter for POST to '{api_path}'")
         elif call_type == "DELETE":
             ret = self._delete(api_path, params)
 
